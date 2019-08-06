d734d0dddf6ff9a2c2821e38b7ff47d9bc1368ba
tsuen4 <ht17a045@oecu.jp>
2019-08-06 17:57:46 +0900

    問題一覧と問題ページを左右に分けた画面を実装

---
+++ web-frontend/src/components/Nav.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="nav">
+    <div id="tutorial-list">
+      <ul>
+        <li v-for="tutorial in tutorials" :key="tutorial.id">
+          <router-link :to="tutorial.contents">{{tutorial.name}}</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  data: () => {
+    return {
+      tutorials: [
+        { id: 0, name: 'Home', contents: '/' },
+        { id: 1, name: 'About', contents: '/about' },
+        { id: 2, name: 'item1', contents: '/id1' },
+        { id: 3, name: 'item2', contents: '/id2' },
+        { id: 4, name: 'item3', contents: '/id3' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ web-frontend/src/components/Tutorial.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="tutorial">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tutorial'
+}
+</script>
+
+<style>
+
+</style>
