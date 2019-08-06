d770b1a3ba98b66d9278e10b9165e7cc037b2786
tsuen4 <ht17a045@oecu.jp>
2019-08-06 21:26:05 +0900

    markdown のファイルを直接読めるように

---
+++ web-frontend/src/views/text/WhatIsTheGit.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="git-text">
+    <vue-markdown :source="source"></vue-markdown>
+  </div>
+</template>
+
+<script>
+import md from '@/assets/text/what-is-the-git.md'
+import VueMarkdown from 'vue-markdown'
+
+export default {
+  components: {
+    VueMarkdown
+  },
+  data: () => {
+    return {
+      // raw データの取得
+      source: md.source
+    }
+  }
+}
+</script>
+
+<style>
+</style>
