ff4caef0a7f4748609556f3932365687caac0ce7
tsuen4 <ht17a045@oecu.jp>
2019-08-06 17:57:46 +0900

    問題一覧と問題ページを左右に分けた画面を実装

---
+++ web-frontend/src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="app">
+    <Nav></Nav>
+    <Tutorial></Tutorial>
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>-->
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+import Nav from '@/components/Nav.vue'
+import Tutorial from '@/components/Tutorial.vue'
+
+export default {
+  components: {
+    Nav,
+    Tutorial
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0px;
+}
+
+#app {
+  padding: 0px;
+  /* width: 1000px; */
+  /* margin: auto; */
+}
+
+div {
+  /* border: 1px dotted #000000; */
+}
+
+#nav {
+  float: left;
+  width: 200px;
+}
+
+#tutorial {
+  margin-left: 200px;
+  padding: 10px;
+  /* width: 780px; */
+  background-color: gainsboro;
+}
+
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+/
+
+#nav {
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+} */
+</style>
---
+++ web-frontend/vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  runtimeCompiler: true,
+  outputDir: '../docs/',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/(RepositoryName)/' : './',
+  publicPath: './'
+}
