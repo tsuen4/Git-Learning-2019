67eda2fc4b5409f4dbccce1bdca6424d8a6f2c40
tsuen4 <ht17a045@oecu.jp>
2019-08-10 02:05:10 +0900

    コミットのやり直しテキストの追加

---
+++ web-frontend/src/views/text/GitAmend.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="git-text">
+    <md class="markdown-body"></md>
+    <TerminalBlock></TerminalBlock>
+  </div>
+</template>
+
+<script>
+import md from '@/assets/text/git-amend.md'
+import TerminalBlock from '@/components/Terminal.vue'
+
+export default {
+  components: {
+    md,
+    TerminalBlock
+  }
+}
+</script>
+
+<style>
+</style>
