0fd022a97517ffdcc93501bc374d45f9e6df9bf3
tsuen4 <ht17a045@oecu.jp>
2019-08-09 16:12:44 +0900

    スタイルの適用

--- web-frontend/src/App.vue
+++ web-frontend/src/App.vue
@@ -52,14 +52,14 @@ body {
   height: 300px;
 }
 
-/* #app {
+#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
+/*
 #nav {
 }
 
