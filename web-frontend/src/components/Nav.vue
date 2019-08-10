18d70a627fee143e0faee4f70c7d97429d2dfa22
tsuen4 <ht17a045@oecu.jp>
2019-08-11 00:20:27 +0900

    v-bind:key の対象を変更

--- web-frontend/src/components/Nav.vue
+++ web-frontend/src/components/Nav.vue
@@ -6,7 +6,7 @@
           tag="li"
           :to="tutorial.contents"
           v-for="tutorial in tutorials"
-          :key="tutorial.id"
+          :key="tutorial.contents"
         >{{tutorial.name}}</router-link>
       </ul>
     </div>
@@ -19,12 +19,12 @@ export default {
   data: () => {
     return {
       tutorials: [
-        { id: 0, name: 'Home', contents: '/' },
-        { id: 1, name: 'Git とは', contents: '/what-is-the-git' },
-        { id: 2, name: 'リポジトリの作成', contents: '/create-repository' },
-        { id: 3, name: '変更内容の記録', contents: '/git-commit' },
-        { id: 4, name: 'ブランチによる分岐と統合', contents: '/branch' },
-        { id: 5, name: 'コミットのやり直し', contents: '/git-amend' }
+        { name: 'Home', contents: '/' },
+        { name: 'Git とは', contents: '/what-is-the-git' },
+        { name: 'リポジトリの作成', contents: '/create-repository' },
+        { name: '変更内容の記録', contents: '/git-commit' },
+        { name: 'ブランチによる分岐と統合', contents: '/branch' },
+        { name: 'コミットのやり直し', contents: '/git-amend' }
       ]
     }
   }
