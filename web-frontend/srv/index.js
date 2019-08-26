0ce84a4d02a0534f158c92ac19f9d6b15caf0a88
tsuen4 <ht17a045@oecu.jp>
2019-08-27 00:01:48 +0900

    ESLint のエラーを修正

--- web-frontend/srv/index.js
+++ web-frontend/srv/index.js
@@ -1,29 +1,29 @@
-"use strict";
-const express = require('express');
-const bodyParser = require('body-parser');
-const build = require('./build');
+'use strict'
+// const express = require('express')
+const bodyParser = require('body-parser')
+const build = require('./build')
 
 export default (app, http) => {
-  build.init(http);
+  build.init(http)
 
-  app.use(bodyParser.json());
+  app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({
     extended: true
-  }));
+  }))
 
-  // app.use('/node_modules', express.static(__dirname + '/node_modules'));
-  // app.use(express.static(__dirname + '/public'));
+  // app.use('/node_modules', express.static(__dirname + '/node_modules'))
+  // app.use(express.static(__dirname + '/public'))
 
   app.post('/api/console/:imagename', async function (req, res) {
     try {
-      const container = await build.run(req.params.imagename);
+      const container = await build.run(req.params.imagename)
       res.send({
         containerId: container.id
-      });
+      })
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
-  });
+  })
   // app.use(express.json())
   //
   // app.get('/foo', (req, res) => {
@@ -33,14 +33,14 @@ export default (app, http) => {
   // app.post('/bar', (req, res) => {
   //   res.json(req.body)
   // })
-  // 
+  //
   // optional support for socket.io
-  // 
+  //
   // let io = socketIO(http)
-  // io.on("connection", client => {
-  //   client.on("message", function(data) {
+  // io.on('connection', client => {
+  //   client.on('message', function(data) {
   //     // do something
   //   })
-  //   client.emit("message", "Welcome")
+  //   client.emit('message', 'Welcome')
   // })
 }
