222305e0cf5ca276d78ed9ead342553387448b22
tsuen4 <ht17a045@oecu.jp>
2019-08-29 01:56:38 +0900

    とりあえずビルドしたDockerコンテナを起動するように

--- web-frontend/src/components/Terminal.vue
+++ web-frontend/src/components/Terminal.vue
@@ -10,7 +10,7 @@ import * as fit from 'xterm/lib/addons/fit/fit'
 export default {
   name: 'Terminal',
   beforeCreate: () => {
-    fetch('/api/console/ubuntu', {
+    fetch('/api/console/gi-al', {
       method: 'POST',
       body: JSON.stringify({}),
       headers: new Headers({ 'Content-type': 'application/json' })
@@ -18,13 +18,14 @@ export default {
       return response.json()
     }).then(function (response) {
       let socket = io.connect('/' + response.containerId)
-      let term = new Terminal({
-        // cols: 120, // Set the terminal's width to 120 columns
-        // rows: 40 // Set the terminal's height to 80 rows
-      })
       Terminal.applyAddon(fit)
+      let term = new Terminal({
+        // cols: 120,
+        // rows: 20
+      })
       term.open(document.getElementById('terminal'))
       term.fit()
+      console.log(term.rows, term.cols)
 
       socket.on('data', data => {
         term.write(data)
