70e2fe0515f730a7ca048d7858f9458e0e7885f6
tsuen4 <ht17a045@oecu.jp>
2019-09-01 17:15:37 +0900

    一括で docker image をビルドするためのシェルスクリプトを作成

---
+++ docker/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker build -t git-create-repository ./create-repository
+docker build -t git-commit ./commit
+docker build -t git-branch ./branch
+# docker build -t git-branch ./branch
+# docker build -t git-branch ./branch
