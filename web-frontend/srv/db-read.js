bd9a3213d534cf95381b1d291ee0c9940526325a
tsuen4 <ht17a045@oecu.jp>
2019-09-09 02:46:01 +0900

    解答者リストを生成できるようにした

--- web-frontend/srv/db-read.js
+++ web-frontend/srv/db-read.js
@@ -1,51 +1,83 @@
 'use strict'
 const table = require('./table')
-const countData = {
+const exerName = ['リポジトリの作成', '変更内容の記録', 'ブランチによる分岐と統合']
+const answerData = {
   CreateRepository: {
-    name: 'リポジトリの作成',
-    exer: [
-      { id: 1, name: 'リポジトリを作成できたか', answer: 0, correct: 0 }
-    ]
+    name: exerName[0],
+    exer: { id: 1, name: 'リポジトリを作成できたか', answer: 0, correct: 0, user: [] }
   },
   GitCommit: {
-    name: '変更内容の記録',
-    exer: [
-      { id: 1, name: 'コミットできたか', answer: 0, correct: 0 }
-    ]
+    name: exerName[1],
+    exer: { id: 1, name: 'コミットできたか', answer: 0, correct: 0, user: [] }
   },
   GitBranch: {
-    name: 'ブランチによる分岐と統合',
-    exer: [
-      { id: 1, name: 'ブランチを切れたか', answer: 0, correct: 0 },
-      { id: 2, name: 'マージできたか', answer: 0, correct: 0 }
-    ]
+    name: exerName[2],
+    exer: { id: 1, name: 'ブランチを切れたか', answer: 0, correct: 0, user: [] }
+  },
+  GitMerge: {
+    name: exerName[2],
+    exer: { id: 1, name: 'マージできたか', answer: 0, correct: 0, user: [] }
+
   }
 }
 
-exports.Counter = async () => {
+const counter = async () => {
   // CreateRepository
   await table.CreateRepository.count({ where: { 'ans': 1 } })
-    .then(c => { countData.CreateRepository.exer[0].answer = c })
-  await table.CreateRepository.count({ where: { 'create_repository': 1 } })
-    .then(c => { countData.CreateRepository.exer[0].correct = c })
+    .then(c => { answerData.CreateRepository.exer.answer = c })
+  await table.CreateRepository.count({ where: { 'correct': 1 } })
+    .then(c => { answerData.CreateRepository.exer.correct = c })
 
   // GitCommit
   await table.GitCommit.count({ where: { 'ans': 1 } })
-    .then(c => { countData.GitCommit.exer[0].answer = c })
-  await table.GitCommit.count({ where: { 'commited': 1 } })
-    .then(c => { countData.GitCommit.exer[0].correct = c })
+    .then(c => { answerData.GitCommit.exer.answer = c })
+  await table.GitCommit.count({ where: { 'correct': 1 } })
+    .then(c => { answerData.GitCommit.exer.correct = c })
 
   // GitBranch
   await table.GitBranch.count({ where: { 'ans': 1 } })
-    .then(c => {
-      countData.GitBranch.exer[0].answer = c
-      countData.GitBranch.exer[1].answer = c
-    })
-  await table.GitBranch.count({ where: { 'branch': 1 } })
-    .then(c => { countData.GitBranch.exer[0].correct = c })
-  await table.GitBranch.count({ where: { 'merge': 1 } })
-    .then(c => { countData.GitBranch.exer[1].correct = c })
+    .then(c => { answerData.GitBranch.exer.answer = c })
+  await table.GitBranch.count({ where: { 'correct': 1 } })
+    .then(c => { answerData.GitBranch.exer.correct = c })
+  // GitMetge
+  await table.GitMerge.count({ where: { 'ans': 1 } })
+    .then(c => { answerData.GitMerge.exer.answer = c })
+  await table.GitMerge.count({ where: { 'correct': 1 } })
+    .then(c => { answerData.GitMerge.exer.correct = c })
 
-  console.log(countData)
-  return countData
+  // console.log(answerData)
+}
+
+const userList = async () => {
+  // 配列を空にして入れ直す
+  await table.CreateRepository.findAll({ order: [['id', 'ASC']] })
+    .then(results => {
+      answerData.CreateRepository.exer.user = []
+      results.forEach(r => answerData.CreateRepository.exer.user.push(r.get()))
+    }
+    )
+  await table.GitCommit.findAll({ order: [['id', 'ASC']] })
+    .then(results => {
+      answerData.GitCommit.exer.user = []
+      results.forEach(r => answerData.GitCommit.exer.user.push(r.get()))
+    }
+    )
+  await table.GitBranch.findAll({ order: [['id', 'ASC']] })
+    .then(results => {
+      answerData.GitBranch.exer.user = []
+      results.forEach(r => answerData.GitBranch.exer.user.push(r.get()))
+    }
+    )
+  await table.GitMerge.findAll({ order: [['id', 'ASC']] })
+    .then(results => {
+      answerData.GitMerge.exer.user = []
+      results.forEach(r => answerData.GitMerge.exer.user.push(r.get()))
+    }
+    )
+}
+
+exports.Counter = async () => {
+  await counter()
+  await userList()
+  return answerData
 }
