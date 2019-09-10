b259633b9f246143e71b36ab8ccb1ea528b16e48
tsuen4 <ht17a045@oecu.jp>
2019-09-10 23:12:05 +0900

    ログイン部をコンポーネントに

--- web-frontend/src/views/Home.vue
+++ web-frontend/src/views/Home.vue
@@ -1,58 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <input type="button" value="Login" @click="signUp()" />
-    <input type="button" value="Logout" @click="signOut()" />
-    <span>{{ status }}</span>
+  <div id="text-view" class="markdown-body">
+    <h1>Git-Learning</h1>
+    <Login />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import firebase from '../firebase'
-import axios from 'axios'
-
+import Login from '@/components/Login.vue'
 export default {
   name: 'home',
   components: {
-    HelloWorld
-  },
-  created: function () {
-    this.onAuth()
-  },
-  beforeDestroy: function () {
-    if (this.$store.getters.isSignedIn) {
-      // createUser
-      axios.post('/api/db/create-user', {
-        id: this.id,
-        name: this.name
-      })
-    }
-    console.log('===== beforeDestroy =====')
-  },
-  computed: {
-    id: function () {
-      return this.$store.getters.id
-    },
-    name: function () {
-      return this.$store.getters.name
-    },
-    status: function () {
-      return this.$store.getters.id
-    }
-  },
-  methods: {
-    signUp: () => {
-      firebase.signUp()
-    },
-    signOut: () => {
-      firebase.signOut()
-    },
-    onAuth: () => {
-      firebase.onAuth()
-    }
+    Login
   }
 }
 </script>
+
+<style>
+</style>
