565313a2a54304acfa38b19bbc5f4b5490d5a00b
tsuen4 <ht17a045@oecu.jp>
2019-09-11 17:09:40 +0900

    採点方法を変更し、エラーメッセージを出力せずに確認できるように

--- docker/shell/create-repository/git-l/git-create-repository_sc.sh
+++ docker/shell/create-repository/git-l/git-create-repository_sc.sh
@@ -6,8 +6,8 @@ id="\"id\": \"${userId}\""
 
 echo -e "\n===== 採点結果 ====="
 
-if ! ls /hello-git/ -a | grep .git -q; then
-    echo "NG: '/hello-edit/' が Git のリポジトリとして初期化されていません。"
+if ! ls /hello-git/.git &> /dev/null; then
+    echo "NG: '/hello-edit/' が作成されていなか、 Git のリポジトリとして初期化されていません。"
     flag=1
     create_repository="\"correct\": 0"
 else
