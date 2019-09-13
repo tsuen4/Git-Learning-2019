275ae493eba1876003e9f96538bb86b33da2621d
tsuen4 <ht17a045@oecu.jp>
2019-09-13 21:33:09 +0900

    命名に統一性がなかったので修正

--- docker/shell/build.sh
+++ docker/shell/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t git-create-repository ./create-repository
+docker build -t create-repository ./create-repository
 docker build -t git-commit ./commit
 docker build -t git-branch ./branch
 # docker build -t git-branch ./branch
