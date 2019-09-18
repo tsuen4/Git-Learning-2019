3087e00d5e1edd457c02b8ca79cbce64a0310c2b
tsuen4 <ht17a045@oecu.jp>
2019-09-18 20:55:11 +0900

    ルーターから GitHub-Flow を削除

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -2,12 +2,6 @@
   <div id="text-view" class="markdown-body">
     <h1>Git-Learning</h1>
     <Login />
-    <div v-if="this.$store.getters.isSignedIn">
-      <input type="radio" id="git" value="git" name="materials" v-model="picked" />
-      <label for="git">Git</label>
-      <input type="radio" id="github" value="github" name="materials" v-model="picked" />
-      <label for="github">GitHub Flow</label>
-    </div>
   </div>
 </template>
 
@@ -17,16 +11,6 @@ export default {
   name: 'home',
   components: {
     Login
-  },
-  computed: {
-    picked: {
-      get: function () {
-        return this.$store.getters.materials
-      },
-      set: function (value) {
-        this.$store.commit('onMaterialChanged', value)
-      }
-    }
   }
 }
 </script>
