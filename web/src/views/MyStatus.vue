51b9b4866635d429930d803f73bb9f8642e31ab3
tsuen4 <ht17a045@oecu.jp>
2019-09-19 20:48:37 +0900

    解答状況を確認できる画面を追加

---
+++ web/src/views/MyStatus.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="text-view" class="markdown-body">
+    <h1>解答状況</h1>
+    <table>
+      <tr>
+        <th>教材名</th>
+        <th>問題名</th>
+        <th>解答状況</th>
+        <th>正誤</th>
+      </tr>
+      <tr v-for="ans in ansData" :key="ans.name && ans.exer">
+        <td>{{ ans.name }}</td>
+        <td>{{ ans.exer }}</td>
+        <td>{{ rw(ans.answer) }}</td>
+        <td>{{ rw(ans.correct) }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'my-status',
+  data: () => {
+    return {
+      ansData: null
+    }
+  },
+  created: function () {
+    axios.post('/api/db/get-my-status', { id: this.$store.getters.id })
+      .then(res => { this.ansData = res.data })
+  },
+  methods: {
+    rw: (num) => {
+      return num === 1 ? '○' : '×'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
