8b006bbb38b50dc7a9cd3816759d097253292f68
tsuen4 <ht17a045@oecu.jp>
2019-09-19 20:48:54 +0900

    教材の概要文を追加

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -2,15 +2,19 @@
   <div id="text-view" class="markdown-body">
     <h1>Git-Learning</h1>
     <Login />
+    <HowToUse />
   </div>
 </template>
 
 <script>
 import Login from '@/components/Login.vue'
+import HowToUse from '@/assets/text/how-to-use.md'
+
 export default {
   name: 'home',
   components: {
-    Login
+    Login,
+    HowToUse
   }
 }
 </script>
