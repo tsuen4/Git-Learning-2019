9867d4f66055f657870d77d45ba9600fe042c463
tsuen4 <ht17a045@oecu.jp>
2019-09-19 13:47:44 +0900

    ボタンをアンカーに変更

--- web/src/components/Login.vue
+++ web/src/components/Login.vue
@@ -3,8 +3,8 @@
     <div v-if="isLoading">loading...</div>
     <div v-else>
       <span v-if="isSignedIn()" class="status">{{ this.$store.getters.id }}: {{ status }}</span>
-      <input v-if="!isSignedIn()" type="button" value="Login" @click="signUp()" />
-      <input v-else type="button" value="Logout" @click="signOut()" />
+      <a v-if="!isSignedIn()" @click="signUp()">Login</a>
+      <a v-else @click="signOut()">Logout</a>
     </div>
   </div>
 </template>
