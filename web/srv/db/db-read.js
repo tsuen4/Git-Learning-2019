aba5ecee22e426e73280d88bb4c5a1991cb702d5
tsuen4 <ht17a045@oecu.jp>
2019-09-19 20:47:51 +0900

    学生個人の解答状況をデータベースから引っ張ってくるように

--- web/srv/db/db-read.js
+++ web/srv/db/db-read.js
@@ -107,3 +107,59 @@ exports.answer = async () => {
   await userList()
   return answerData
 }
+
+exports.myAnswer = async userId => {
+  const myAnsData = {
+    create_repository: {
+      name: tutorialName[0],
+      exer: exerName[0],
+      answer: 0,
+      correct: 0,
+    },
+    commit: {
+      name: tutorialName[1],
+      exer: exerName[1],
+      answer: 0,
+      correct: 0,
+    },
+    branch: {
+      name: tutorialName[2],
+      exer: exerName[2],
+      answer: 0,
+      correct: 0,
+    },
+    merge: {
+      name: tutorialName[2],
+      exer: exerName[3],
+      answer: 0,
+      correct: 0,
+    },
+    amend: {
+      name: tutorialName[3],
+      exer: exerName[4],
+      answer: 0,
+      correct: 0,
+    },
+    no_edit: {
+      name: tutorialName[3],
+      exer: exerName[5],
+      answer: 0,
+      correct: 0,
+    },
+    checkout: {
+      name: tutorialName[4],
+      exer: exerName[6],
+      answer: 0,
+      correct: 0,
+    }
+  }
+  await table.Answers.findAll({ where: { id: userId } })
+    .then(results => {
+      results.forEach(r => {
+        let data = r.get()
+        myAnsData[data.exer].answer = data.answer
+        myAnsData[data.exer].correct = data.correct
+      })
+    })
+  return myAnsData
+}
