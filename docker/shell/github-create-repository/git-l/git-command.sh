2dabc69ff6ba1fa9879ff22ae82f2e03ec3276c7
tsuen4 <ht17a045@oecu.jp>
2019-09-20 11:59:25 +0900

    GitHubにリポジトリ作る問題の作成

---
+++ docker/shell/github-create-repository/git-l/git-command.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+git config --global user.email "${userId}"
+git config --global user.name "${userName}"
+
+mkdir /hello-git
+cd /hello-git
+git init
+
+echo "hello" > hello.txt
+git add hello.txt
+git commit -m "はじめてのコミット"
+
+git checkout -b edit-hello
+echo "edit on edit-hello branch" >> hello.txt
+git add hello.txt
+git commit -m "edit-hello ブランチでのコミット"
+git checkout master
+git merge edit-hello
+git branch -d edit-hello
