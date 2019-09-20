36eae916131be0b2a0cca74fb614338efaae130d
tsuen4 <ht17a045@oecu.jp>
2019-09-20 11:59:39 +0900

    [WIP] GitHubにリポジトリ作る問題の採点用シェルスクリプト

---
+++ docker/shell/github-create-repository/git-l/__github-create-repository_sc.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+count=0
+flag=0
+id="\"id\": \"${userId}\""
+
+echo -e "\n===== 採点結果 ====="
+
+if ! git branch | grep edit-hello -q; then
+    echo "NG: 'edit-hello' ブランチが作成されていません。"
+    flag=1
+    branch="\"correct\": 0"
+else
+    echo "OK: 'edit-hello' ブランチが作成されています。"
+    let ++count
+    branch="\"correct\": 1"
+fi
+
+if ! git log --decorate | grep 'master, edit-hello' -q; then
+    echo "NG: 'edit-hello' ブランチが master ブランチにマージされていません。"
+    flag=1
+    merge="\"correct\": 0"
+else
+    echo "OK: 'edit-hello' ブランチが master ブランチにマージされています。"
+    let ++count
+    merge="\"correct\": 1"
+fi
+
+echo -e "\n正解数: $count/2"
+
+curl -X POST 192.168.163.225/api/scoring/branch -H "Content-Type: application/json" -d "{${id}, ${branch}}"
+curl -X POST 192.168.163.225/api/scoring/merge -H "Content-Type: application/json" -d "{${id}, ${merge}}"
+
+exit $flag
