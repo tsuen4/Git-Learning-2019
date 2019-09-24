eeedfbf8aff9310ad13e8e44936d28aabba28303
tsuen4 <ht17a045@oecu.jp>
2019-09-24 12:03:46 +0900

    TAを管理者に追加

--- web/srv/users.js
+++ web/srv/users.js
@@ -8,6 +8,14 @@ const users = [
   {
     email: 'hisamatu@osakac.ac.jp',
     role: 'admin'
+  },
+  {
+    email: 'mt18a003@oecu.jp',
+    role: 'admin'
+  },
+  {
+    email: 'mt18a006@oecu.jp',
+    role: 'admin'
   }
 ]
 
