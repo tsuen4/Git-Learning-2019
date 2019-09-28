87accfbf71918d63b45bbedeb6f63db617a01a24
tsuen4 <etsushigs3052@gmail.com>
2019-09-28 10:22:39 +0000

    docker-compose から教材用シェルを隔離

--- docker/shell/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker build -t create-repository ./create-repository
-docker build -t git-commit ./commit
-docker build -t git-branch ./branch
-docker build -t git-amend ./amend
-docker build -t git-checkout ./checkout
-docker build -t github-create-repository ./github-create-repository
