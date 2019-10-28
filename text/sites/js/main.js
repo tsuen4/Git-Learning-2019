bbcfb742c9254d02ef28629a0726acd2679e8fce
tsuen4 <ht17a045@oecu.jp>
2019-10-29 00:35:48 +0900

    JS 版も Vue.js で作ったものと同じ仕様にした

--- text/sites/js/main.js
+++ text/sites/js/main.js
@@ -1,39 +1,52 @@
 let todoItem = [
-  { id: 0, text: 'todo1', checked: false },
-  { id: 1, text: 'todo2', checked: false },
-  { id: 2, text: 'todo3', checked: false },
+  // { id: 0, text: 'todo1', checked: false }
 ]
 
-let count = todoItem.length;
-const todo_ul = document.getElementById('todo_ul');
+let count = todoItem.length
+const todo_ul = document.getElementById('todo_ul')
+
+const checkStorage = () => {
+  if (localStorage.getItem('todo')) {
+    try {
+      todoItem = JSON.parse(localStorage.getItem('todo'))
+    } catch (e) {
+      localStorage.removeItem('todo')
+    }
+  }
+}
+
+const saveTodo = () => {
+  const parsed = JSON.stringify(todoItem)
+  localStorage.setItem('todo', parsed)
+}
 
 const reGenerate = () => {
+  // html 側のリストを全消去
   while (todo_ul.firstChild) {
-    todo_ul.removeChild(todo_ul.firstChild);
+    todo_ul.removeChild(todo_ul.firstChild)
   }
+
+  // 再生成
   for (const item of todoItem) {
-    const listEl = document.createElement('li');
-    listEl.setAttribute('id', 'todo-item');
+    const listEl = document.createElement('li')
+    listEl.setAttribute('class', 'todo-item')
 
-    const labelEl = document.createElement('label');
-    labelEl.textContent = item.text;
+    const checkEl = document.createElement('input')
+    checkEl.setAttribute('type', 'checkbox')
+    if (item.checked === true) { checkEl.checked = true }
+    checkEl.setAttribute('onclick', `checkItem(${item.id})`)
 
-    const checkEl = document.createElement('input');
-    checkEl.setAttribute('type', 'checkbox');
-    if (item.checked === true) {
-      checkEl.checked = true;
-    }
-    checkEl.setAttribute('onclick', `checkItem(${item.id})`);
-
-    listEl.appendChild(labelEl);
-    labelEl.appendChild(checkEl);
-    todo_ul.appendChild(listEl);
+    const labelEl = document.createElement('label')
+    
+    labelEl.appendChild(checkEl)
+    labelEl.insertAdjacentHTML('beforeend', item.text)
+    listEl.appendChild(labelEl)
+    todo_ul.appendChild(listEl)
   }
-  console.log(todoItem);
+  // console.log(todoItem)
 }
 
 const checkItem = (id) => {
-  // console.log(`${id}: ${checkState}`);
   for (let i = 0; i < todoItem.length; i++) {
     if (todoItem[i].id === id && todoItem[i].checked) {
       todoItem[i].checked = false
@@ -41,33 +54,42 @@ const checkItem = (id) => {
       todoItem[i].checked = true
     }
   }
+  saveTodo()
 }
 
 const addTodo = (press) => {
-  const todo = document.getElementById('todo_value');
+  const todo = document.getElementById('todo_value')
 
-  if (!todo.value) return false;
+  if (!todo.value) return false
   // ボタン(true) 又は Enter が押されたら
   if (window.event.keyCode == 13 || press == true) {
+    const nextId = todoItem.length === 0 ?
+      0 :
+      todoItem[todoItem.length - 1].id + 1
     todoItem.push({
-      id: count++,
+      id: nextId,
       text: todo.value,
       checked: false
     })
-    reGenerate();
-    todo.value = '';
+    reGenerate()
+    todo.value = ''
   }
+  saveTodo()
 }
 
 const removeTodo = () => {
   todoItem = todoItem.filter((element, index, array) => {
-    return Boolean(element.checked == false);
+    return Boolean(element.checked == false)
   })
-  reGenerate();
+  reGenerate()
+  saveTodo()
 }
 
-window.checkItem = checkItem;
-window.addTodo = addTodo;
-window.removeTodo = removeTodo;
+window.checkItem = checkItem
+window.addTodo = addTodo
+window.removeTodo = removeTodo
 
-window.onload = () => { reGenerate(); }
+window.onload = () => {
+  checkStorage()
+  reGenerate()
+}
