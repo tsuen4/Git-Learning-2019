61a2078897dd7dcda16423c45027ac626617709c
tsuen4 <ht17a045@oecu.jp>
2019-10-28 23:44:01 +0900

    アイテムを localStorage に保存する仕様にした

--- text/sites/vue/main.js
+++ text/sites/vue/main.js
@@ -4,26 +4,45 @@ const todo = new Vue({
     return {
       inputText: '',
       todoItem: [
-        { id: 0, text: 'todo1', checked: false },
-        { id: 1, text: 'todo2', checked: false },
-        { id: 2, text: 'todo3', checked: false }
+        // { id: 0, text: 'todo1', checked: false },
+        // { id: 1, text: 'todo2', checked: false },
+        // { id: 2, text: 'todo3', checked: false }
       ]
+    }
+  },
+  mounted () {
+    if (localStorage.getItem('todo')) {
+      try {
+        this.todoItem = JSON.parse(localStorage.getItem('todo'))
+      } catch (e) {
+        localStorage.removeItem('todo')
+      }
     }
   },
   methods: {
     addTodo: function () {
       if (this.inputText === '') return false
+      
+      const nextId = this.todoItem.length === 0 ?
+        0 :
+        this.todoItem[this.todoItem.length - 1].id + 1
       this.todoItem.push({
-        id: this.todoItem[this.todoItem.length - 1].id + 1,
+        id: nextId,
         text: this.inputText,
         checked: false
       })
       this.inputText = ''
+      this.saveTodo()
     },
     removeTodo: function () {
       this.todoItem = this.todoItem.filter((element, index, array) => {
-        return Boolean(element.checked == false);
+        return Boolean(element.checked == false)
       })
+      this.saveTodo()
+    },
+    saveTodo: function () {
+      const parsed = JSON.stringify(this.todoItem)
+      localStorage.setItem('todo', parsed)
     }
   }
 })
