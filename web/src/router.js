a1384f4029ce6569f7c473c1368b4b38bed84e15
tsuen4 <ht17a045@oecu.jp>
2020-08-14 22:02:06 +0900

    不要になったルートとコンポーネントを削除

--- web/src/router.js
+++ web/src/router.js
@@ -33,41 +33,6 @@ const router = new Router({
       path: '/my-status',
       name: 'my-status',
       component: () => import(/* webpackChunkName: "my-status" */ './views/MyStatus.vue')
-    },
-    {
-      path: '/what-is-the-git',
-      name: 'what-is-the-git',
-      component: () => import(/* webpackChunkName: "what-is-the-git" */ './views/text/what-is-the-git.vue')
-    },
-    {
-      path: '/create-repository',
-      name: 'create-repository',
-      component: () => import(/* webpackChunkName: "create-repository" */ './views/text/create-repository.vue')
-    },
-    {
-      path: '/commit',
-      name: 'commit',
-      component: () => import(/* webpackChunkName: "commit" */ './views/text/commit.vue')
-    },
-    {
-      path: '/branch',
-      name: 'branch',
-      component: () => import(/* webpackChunkName: "branch" */ './views/text/branch.vue')
-    },
-    {
-      path: '/amend',
-      name: 'amend',
-      component: () => import(/* webpackChunkName: "amend" */ './views/text/amend.vue')
-    },
-    {
-      path: '/checkout',
-      name: 'checkout',
-      component: () => import(/* webpackChunkName: "checkout" */ './views/text/checkout.vue')
-    },
-    {
-      path: '/github-create-repository',
-      name: 'github-create-repository',
-      component: () => import(/* webpackChunkName: "github-create-repository" */ './views/text/github-create-repository.vue')
     }
   ],
   // スクロール位置を保存 & 保存されていないページは一番上に
