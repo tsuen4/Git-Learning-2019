55979b510bc8e499449da17abacda23e1f847588
tsuen4 <ht17a045@oecu.jp>
2020-08-14 22:34:18 +0900

    トップページのテキストもサーバーから取得するように

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -2,19 +2,19 @@
   <div id="text-view" class="markdown-body">
     <h1>Git-Learning</h1>
     <Login />
-    <HowToUse />
+    <TextComponent tutorial-name="how-to-use" />
   </div>
 </template>
 
 <script>
 import Login from '@/components/Login.vue'
-import HowToUse from '@/assets/text/how-to-use.md'
+import TextComponent from '@/components/Text.vue'
 
 export default {
   name: 'home',
   components: {
     Login,
-    HowToUse
+    TextComponent
   }
 }
 </script>
