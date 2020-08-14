8b2fce152a52b847d45877bc1855a93d27fb7839
tsuen4 <ht17a045@oecu.jp>
2020-08-14 18:49:15 +0900

    marked.js で Markdown から HTML に変換

--- web/src/components/Text.vue
+++ web/src/components/Text.vue
@@ -1,11 +1,10 @@
 <template>
-  <div class="markdown-body">
-    {{ receivedText }}
-  </div>
+  <div class="markdown-body" v-html="compiledMarkdown" />
 </template>
 
 <script>
 import axios from 'axios'
+import marked from 'marked'
 
 export default {
   props: {
@@ -28,10 +27,12 @@ export default {
       .catch(res => {
         // console.error(res)
       })
+  },
+  computed: {
+    compiledMarkdown () { return marked(this.receivedText) }
   }
 }
 </script>
 
 <style>
-
 </style>
