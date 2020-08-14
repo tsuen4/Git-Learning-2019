ec579258638425d6cdb5a0a55bea97b5966fe7cf
tsuen4 <ht17a045@oecu.jp>
2020-08-14 22:49:43 +0900

    使わなくなったモジュールを削除 (markdown-to-vue-loader)

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,40 +1,9 @@
-const hljs = require('highlight.js')
-
 module.exports = {
   runtimeCompiler: true,
   // outputDir: '../docs/',
 
   // publicPath: process.env.NODE_ENV === 'production' ? '/(RepositoryName)/' : './',
   publicPath: '/tutorial/',
-
-  configureWebpack: {
-    module: {
-      rules: [
-        {
-          test: /\.md$/,
-          exclude: /node_modules/,
-          use: [
-            'vue-loader',
-            {
-              loader: 'markdown-to-vue-loader',
-              options: {
-                markdownItOptions: {
-                  highlight: function (str, lang) {
-                    if (lang && hljs.getLanguage(lang)) {
-                      try {
-                        return hljs.highlight(lang, str).value
-                      } catch (__) { }
-                    }
-                    return '' // use external default escaping
-                  }
-                }
-              }
-            }
-          ]
-        }
-      ]
-    }
-  },
 
   pluginOptions: {
     express: {
