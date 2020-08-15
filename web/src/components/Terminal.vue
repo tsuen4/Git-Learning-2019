cf7a2cca00d7bef838113032a5acabd7d813bfdc
tsuen4 <ht17a045@oecu.jp>
2020-08-15 12:46:44 +0900

    イメージ名の変更に対応

--- web/src/components/Terminal.vue
+++ web/src/components/Terminal.vue
@@ -14,8 +14,6 @@ import io from 'socket.io-client'
 import { Terminal } from 'xterm'
 import 'xterm/css/xterm.css'
 
-let socket
-
 export default {
   name: 'Terminal',
   props: {
@@ -24,48 +22,58 @@ export default {
   data () {
     return {
       userId: '',
-      userName: ''
+      userName: '',
+      socket: io
     }
   },
   created () {
     this.userId = this.$store.getters.id
     this.userName = this.$store.getters.name
 
-    fetch(`/tutorial/api/console/${this.imageName}`, {
-      method: 'POST',
-      body: JSON.stringify({ userId: this.userId, userName: this.userName }),
-      headers: new Headers({ 'Content-type': 'application/json' })
-    }).then(response => response.json())
-      .then(response => {
-        socket = io.connect(`/tutorial/${response.containerId}`, { path: '/tutorial/socket.io' })
-        const term = new Terminal({
-          rows: 18,
-          cols: 80
-        })
-        term.open(document.getElementById('terminal'))
-
-        // コンテナとのデータのやり取り
-        socket.on('data', data => {
-          term.write(data)
-        })
-        term.onData(data => {
-          socket.emit('data', data)
-        })
-
-        // 接続が切れたときの処理
-        socket.on('disconnect', () => {
-          term.dispose()
-        })
-      })
+    this.connectContainer(this.imageName)
   },
   methods: {
     scoring () {
-      socket.emit('data', 'q') // less 抜け用の q
-      socket.emit('data', '\x03') // ^C を送信
-      socket.emit('data', `${this.imageName}_sc.sh\n`)
+      this.socket.emit('data', 'q') // less 抜け用の q
+      this.socket.emit('data', '\x03') // ^C を送信
+      this.socket.emit('data', `${this.imageName}_sc.sh\n`)
     },
     reload () {
       this.$router.go({ path: this.$router.currentRoute.path, force: true })
+    },
+    connectContainer (imageName) {
+      fetch(`/tutorial/api/console/${this.imageName}`, {
+        method: 'POST',
+        body: JSON.stringify({ userId: this.userId, userName: this.userName }),
+        headers: new Headers({ 'Content-type': 'application/json' })
+      }).then(response => response.json())
+        .then(response => {
+          this.socket = io.connect(`/tutorial/${response.containerId}`, { path: '/tutorial/socket.io' })
+          const term = new Terminal({
+            rows: 18,
+            cols: 80
+          })
+          term.open(document.getElementById('terminal'))
+
+          // コンテナとのデータのやり取り
+          this.socket.on('data', data => {
+            term.write(data)
+          })
+          term.onData(data => {
+            this.socket.emit('data', data)
+          })
+
+          // 接続が切れたときの処理
+          this.socket.on('disconnect', () => {
+            term.dispose()
+          })
+        })
+    }
+  },
+  watch: {
+    imageName (changedName) {
+      this.socket.disconnect()
+      this.connectContainer(changedName)
     }
   }
 }
