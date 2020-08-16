fca63804841d43b7e23dc9a6f5d1cafc72ddfb19
tsuen4 <ht17a045@oecu.jp>
2020-08-16 14:37:04 +0900

    ボタンコンポーネントの作成

---
+++ web/src/components/Button.vue
@@ -0,0 +1,32 @@
+<template>
+  <button>
+    <slot>
+    </slot>
+  </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+button {
+  font-size: 16px;
+  /* display: inline-block; */
+  background-color: #fff;
+  border: solid 1px #f05033;
+  color: #f05033;
+  border-radius: 0.25rem 0.25rem;
+}
+
+button:hover {
+  background-color: #f05033;
+  color: #fff;
+}
+
+button:active {
+  background-color: #af250d;
+}
+</style>
